6bc1f99b4b9d74774f4e25ead0ad6c07c833c97a
yesol <maniac970430@naver.com>
2023-10-11 23:58:00 +0900

    :lipstick:Home 컴포넌트 구성요소 스타일링
    최상단 Lang 변경 구현, 하단 버튼 애니메이션, 글자 크기 조정, 메인 이미지 사이즈 및 border-radius 조정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Home from "./pages/Home";
 
 import "./App.css";
-import SideMenu from "./components/SideMenu";
-
 function App() {
   return (
     <div className="App">
-      <SideMenu />
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,43 @@
+import { useEffect, useState } from "react";
 import Card from "../components/Card";
 
 import "./Home.css";
 
+const lang_arr = ["JAVASCRIPT", "HTML", "CSS", "REACT.JS"];
+
 const Home = () => {
+  const [lang, setLang] = useState(lang_arr[0]);
+
+  useEffect(() => {
+    let lang_num = 0;
+    const timer = setInterval(() => {
+      if (lang_num > 3) {
+        lang_num = 0;
+      } else {
+        lang_num++;
+      }
+      setLang(lang_arr[lang_num]);
+    }, 900);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="home">
-      <h2>I interested in JAVASCRIPT</h2>
-      <Card>
+      <h2>
+        I interested in <span className={`lang ${lang}`}>{lang}</span>
+      </h2>
+      <Card className="home-card">
         <img src={process.env.PUBLIC_URL + "assets/images/snoopy.jpg"} />
       </Card>
-      <h3>윤예솔</h3>
-      <div>
-        <p>상상을 실제 코드로 구현하는 과정을 즐깁니다.</p>
-        <p>지식의 습득과 성장에 열정을 가지고 있습니다.</p>
+      <div className="intro">
+        <h3>윤예솔</h3>
+        <div className="description">
+          <p>상상을 실제 코드로 구현하는 과정을 즐깁니다.</p>
+          <p>지식의 습득과 성장에 열정을 가지고 있습니다.</p>
+        </div>
+      </div>
+      <div className="detail-btn-wrapper">
+        <button className="detail-btn">↓</button>
       </div>
     </div>
   );
