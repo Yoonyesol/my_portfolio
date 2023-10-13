afc79a445b99c61db265e19d1dc928aa0c446fd0
yesol <maniac970430@naver.com>
2023-10-13 10:59:11 +0900

    ✨Emailjs를 이용한 이메일 전송 구현

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,38 @@
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import Card from "../components/Card";
+
 import "./Contact.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmailHandler = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.message);
+          alert(
+            "이메일 전송이 완료되었습니다! 빠른 시일 내에 답장 드리겠습니다."
+          );
+          form.current.name.value = "";
+          form.current.email.value = "";
+          form.current.message.value = "";
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+  };
+
   return (
     <div className="contact">
       <h1 className="contact-title">
@@ -14,17 +45,17 @@ const Contact = () => {
       </p>
       <div className="contact-card-wrapper">
         <Card className="contact-card">
-          <div className="contents">
+          <form className="contents" ref={form} onSubmit={sendEmailHandler}>
             <h3>Name</h3>
-            <input type="text" />
+            <input type="text" name="name" />
             <h3>Email</h3>
-            <input type="email" />
+            <input type="email" name="email" />
             <h3>Message</h3>
-            <textarea />
-          </div>
-          <div className="send-btn">
-            <button>Send</button>
-          </div>
+            <textarea name="message" />
+            <div className="send-btn">
+              <button type="submit">Send</button>
+            </div>
+          </form>
         </Card>
       </div>
     </div>
