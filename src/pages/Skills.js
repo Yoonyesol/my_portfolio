3ccb20ca93d759554fe472669dd26e8d582fa58e
yesol <maniac970430@naver.com>
2023-10-14 21:42:35 +0900

    💄Skills 컴포넌트 내용 추가 및 CSS 스타일링

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -5,35 +5,50 @@ import "./Skills.css";
 const Skills = forwardRef((props, ref) => {
   return (
     <div className="skills" ref={ref}>
-      <h1 className="skills-title">Tech Skills</h1>
+      <h1 className="skills-main-title">Tech Skills</h1>
       <div className="skill-card-wrapper">
-        <Card className="frontend-card">
-          <h2 className="frontend-title">Frontend</h2>
-          <div className="skill-img-wrapper">
-            <div className="skill-div">
-              <img src={process.env.PUBLIC_URL + "assets/images/html.png"} />
-              <p>HTML5</p>
-            </div>
-            <div className="skill-div">
-              <img src={process.env.PUBLIC_URL + "assets/images/css.png"} />
-              <p>CSS3</p>
-            </div>
-            <div className="skill-div">
-              <img src={process.env.PUBLIC_URL + "assets/images/js.png"} />
-              <p>JAVASCRIPT</p>
-            </div>
-            <div className="skill-div">
+        <div>
+          <Card className="skill-card">
+            <h2 className="skill-title">Frontend</h2>
+            <div className="skill-img-wrapper">
+              <img
+                src={process.env.PUBLIC_URL + "assets/images/htmlcssjs.png"}
+              />
               <img src={process.env.PUBLIC_URL + "assets/images/react.png"} />
-              <p>REACT</p>
+              <img
+                src={process.env.PUBLIC_URL + "assets/images/styled.png"}
+                style={{ width: "120px" }}
+              />
             </div>
-          </div>
-        </Card>
-        <Card className="backend-card">
-          <h2 className="backend-title">Backend</h2>
-        </Card>
-        <Card className="etc-card">
-          <h2 className="etc-title">Etc</h2>
-        </Card>
+          </Card>
+          <Card className="skill-card">
+            <h2 className="skill-title">Backend</h2>
+            <div className="skill-img-wrapper">
+              <img src={process.env.PUBLIC_URL + "assets/images/node.png"} />
+              <img src={process.env.PUBLIC_URL + "assets/images/mysql.png"} />
+            </div>
+          </Card>
+        </div>
+        <div className="right-items">
+          <Card className="skill-card">
+            <h2 className="skill-title">Deployment</h2>
+            <div className="skill-img-wrapper">
+              <img
+                src={process.env.PUBLIC_URL + "assets/images/firebase.png"}
+              />
+              <img src={process.env.PUBLIC_URL + "assets/images/netlify.png"} />
+              <img src={process.env.PUBLIC_URL + "assets/images/heroku.png"} />
+            </div>
+          </Card>
+          <Card className="skill-card">
+            <h2 className="skill-title">Etc</h2>
+            <div className="skill-img-wrapper">
+              <img src={process.env.PUBLIC_URL + "assets/images/git.png"} />
+              <img src={process.env.PUBLIC_URL + "assets/images/github.png"} />
+              <img src={process.env.PUBLIC_URL + "assets/images/python.png"} />
+            </div>
+          </Card>
+        </div>
       </div>
     </div>
   );
