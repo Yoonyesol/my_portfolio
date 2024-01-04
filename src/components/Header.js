f835f57842fb3ca45300506d257e1e6aeac67708
Yoonyesol <maniac970430@naver.com>
2024-01-04 21:36:10 +0900

    💄style: 헤더 반응형으로 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./Header.css";
 
 const Header = ({
@@ -9,37 +10,65 @@ const Header = ({
   moveToContact,
   selectedPage,
 }) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggleMenu = (prev) => {
+    setToggleMenu((prev) => !toggleMenu);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (toggleMenu) {
+        setToggleMenu(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [toggleMenu]);
+
   return (
     <header className="header">
       <div className="header-contents">
-        <div className="header-title" onClick={moveToHome}>
-          Portfolio
+        <div className="title-toggle">
+          <span className="header-title" onClick={moveToHome}>
+            Portfolio
+          </span>
+          <div className="toggle-button">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                `/assets/images/${toggleMenu ? `cancle` : `menu`}.png`
+              }
+              onClick={handleToggleMenu}
+            />
+          </div>
         </div>
-        <div className="menu-list">
-          <div
+        <ul className={`menu-list ${toggleMenu ? "visible" : ""}`}>
+          <li
             className={`menu-item ${selectedPage === "archiving" && "visible"}`}
           >
-            <div onClick={moveToArc}>Archiving</div>
-          </div>
-          <div
-            className={`menu-item ${selectedPage === "skills" && "visible"}`}
-          >
-            <div onClick={moveToSkill}>Tech Skills</div>
-          </div>
-          <div
+            <div onClick={moveToArc}>ARCHIVING</div>
+          </li>
+          <li className={`menu-item ${selectedPage === "skills" && "visible"}`}>
+            <div onClick={moveToSkill}>SKILLS</div>
+          </li>
+          <li
             className={`menu-item ${selectedPage === "project" && "visible"}`}
           >
-            <div onClick={moveToPrj}>Project</div>
-          </div>
-          <div className={`menu-item ${selectedPage === "edu" && "visible"}`}>
-            <div onClick={moveToEdu}>Edu & Cert</div>
-          </div>
-          <div
+            <div onClick={moveToPrj}>PROJECT</div>
+          </li>
+          <li className={`menu-item ${selectedPage === "edu" && "visible"}`}>
+            <div onClick={moveToEdu}>QUALIFICATION</div>
+          </li>
+          <li
             className={`menu-item ${selectedPage === "contact" && "visible"}`}
           >
-            <div onClick={moveToContact}>Contact</div>
-          </div>
-        </div>
+            <div onClick={moveToContact}>CONTACT</div>
+          </li>
+        </ul>
       </div>
     </header>
   );
